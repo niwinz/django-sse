0c6cfe8ea83adb31f0d2718d89fb39bb5c583089
Andrei Antoukh <niwi@niwi.be>
2012-01-11 19:02:03 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+description="""
+HTML5 Server-Side Events for Django 1.3, 1.4+
+"""
+
+long_description = """"""
+
+
+setup(
+    name="django-sse",
+    version=':versiontools:django_sse:',
+    url='https://github.com/niwibe/django-sse',
+    license='BSD',
+    platforms=['OS Independent'],
+    description = description.strip(),
+    long_description = long_description.strip(),
+    author = 'Andrei Antoukh',
+    author_email = 'niwi@niwi.be',
+    maintainer = 'Andrei Antoukh',
+    maintainer_email = 'niwi@niwi.be',
+    packages = [
+        'django_sse',
+    ],
+    include_package_data = True,
+    install_requires=[
+        'distribute',
+    ],
+    setup_requires = [
+        'versiontools >= 1.8',
+    ],
+    zip_safe = False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
